8c4092f07c3d9f9783b87b581704b46721a36a6f
Samuel Berkun <sberkun@berkeley.edu>
2021-02-14 23:23:12 -0800

    clean up generate.py

--- generate.py
+++ generate.py
@@ -1,12 +1,8 @@
 def fact(n):
-    if n==0:
-        return 1
-    return n*fact(n-1)
-
+    return 1 if n==0 else n*fact(n-1)
 
 def match_arm(n):
     return '{0} => "{1}",'.format(n, fact(n))
-
 
 for a in range(171):
     print(match_arm(a))
